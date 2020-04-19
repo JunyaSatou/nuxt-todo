58a0208477c5663596bceb1540ae2ea6b6b6f79b
Junya Sato <sato@metaps-payment.com>
2020-04-19 16:40:53 +0900

    section10 Nuxt.js & FirebaseによるSPA開発入門を学習

---
+++ store/todos.js
@@ -0,0 +1,40 @@
+import firebase from "~/plugins/firebase"
+import {firestoreAction} from 'vuexfire'
+
+// データベースの設定
+const db = firebase.firestore();
+// コレクションはtodosを使用する設定とする
+const todosRef = db.collection('todos');
+
+export const state = () => ({
+  todos: []
+});
+
+export const actions = {
+  init: firestoreAction(({ bindFirestoreRef}) => {
+    bindFirestoreRef('todos', todosRef)
+  }),
+  add: firestoreAction((context, name) => {
+    if(name.trim()) {
+      todosRef.add({
+        name: name,
+        done: false,
+        created: firebase.firestore.FieldValue.serverTimestamp()
+      })
+    }
+  }),
+  remove: firestoreAction((context, id) => {
+    todosRef.doc(id).delete();
+  }),
+  toggle: firestoreAction((context, todo) => {
+    todosRef.doc(todo.id).update({
+      done: !todo.done
+    })
+  })
+};
+
+export const getters = {
+  orderdTodos: state => {
+    return _.sortBy(state.todos, 'created')
+  }
+}
